8556c95b854c0cc5e9b40d5bb287257765b8133c
Martin Lenart <martin_lenart@icloud.com>
2022-12-27 20:08:41 +0100

    Update

--- ADOPMAUI_ExerciseAnswers/ViewModels/Lesson03aViewModel.cs
+++ ADOPMAUI_ExerciseAnswers/ViewModels/Lesson03aViewModel.cs
@@ -1,4 +1,4 @@
-﻿//using ADOPMAUI_Exercise.Views.Lesson03;
+﻿using ADOPMAUI_Exercise.Views.Lesson03;
 using Microsoft.Maui.Controls;
 
 namespace ADOPMAUI_Exercise.ViewModels
@@ -22,51 +22,20 @@ namespace ADOPMAUI_Exercise.ViewModels
         {
             All = new List<Lesson03aViewModel>
             {
-/*                
-                // Part 1. Getting Started with XAML
-                new Lesson02aViewModel(typeof(HelloXamlPage), "Hello, XAML",
-                                      "Display a Label with many properties set"),
-
-                new Lesson02aViewModel(typeof(XamlPlusCodePage), "XAML + Code",
-                                      "Interact with a Slider and Button"),
-
-                // Part 2. Essential XAML Syntax
-                new Lesson02aViewModel(typeof(GridDemoPage), "Grid Demo",
-                                      "Explore XAML syntax with the Grid"),
-
-                 // Part 3. XAML Markup Extensions
-                new Lesson02aViewModel(typeof(SharedResourcesPage), "Shared Resources",
-                                      "Using resource dictionaries to share resources"),
-
-
-                new Lesson02aViewModel(typeof(StaticDemoPage), "x:Static Demo",
-                                      "Using the x:Static markup extensions"),
-
-                new Lesson02aViewModel(typeof(TypeDemoPage), "x:Type Demo",
-                                      "Associate Buttons with a Type"),
-
-                new Lesson02aViewModel(typeof(ReferenceDemoPage), "x:Reference Demo",
-                                      "Reference named elements on the page in Binding"),
-
-                new Lesson02aViewModel(typeof(OnPlatformDemoPage), "OnPlatform Demo",
-                        "Specify values per platform"),
- 
-                new Lesson02aViewModel(typeof(OnIdiomDemoPage), "OnIdiom Demo",
-                        "Specify values per idiom"),
-
-              
-                new Lesson02aViewModel(typeof(SliderBindingsPage), "Slider Bindings",
-                                      "Bind properties of two views on the page"),
-
-                new Lesson02aViewModel(typeof(SliderTransformsPage), "Slider Transforms",
-                                      "Use Sliders with bindings"),
-
-                 new Lesson02aViewModel(typeof(StringFormattingPage), "String Formatting",
-                                      "Use standard .NET formatting specfications"),
-                 
-                 new Lesson02aViewModel(typeof(MultiBindingStringFormatPage), "String Formatting",
-                                      "Combine strings from a MultiBinding"),
-*/
+                    new Lesson03aViewModel(typeof(StackLayoutExploration), "StackLayout Exploration",
+                                      "Explore StackLayout before the exercise"),
+                    new Lesson03aViewModel(typeof(StackLayoutExercise), "StackLayout Exercise",
+                                      "Try your skills in this exercise"),
+                    new Lesson03aViewModel(typeof(ScrollViewExercise), "ScrollView Exercise",
+                                      "Have fun with ScrollView"),
+                    new Lesson03aViewModel(typeof(AbsoluteLayoutExploration), "AbsoluteLayout Exploration",
+                                      "Explore AbsoluteLayout before the exercise"),
+                    new Lesson03aViewModel(typeof(AbsoluteLayoutExercise), "AbsoluteLayout Exercise",
+                                      "Have fun with AbsoluteLayout"),
+                    new Lesson03aViewModel(typeof(GridLayoutExploration), "GridLayout Exploration",
+                                      "Explore GridLayout before the exercise"),
+                    new Lesson03aViewModel(typeof(GridLayoutExercise), "GridLayout Exercise",
+                                      "Have fun with GridLayout"),
             };
         }
 
